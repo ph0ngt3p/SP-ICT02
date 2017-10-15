d33b82c6df2acec7220aa8d835896e9975a7a18e
Tuấn Dũng <zun1903@gmail.com>
2017-10-15 17:29:42 +0700

    added eslint pre-commit

--- app/models/Wears.js
+++ app/models/Wears.js
@@ -3,13 +3,13 @@
 const mongoose = require('mongoose')
 
 const wearSchema = new mongoose.Schema({
-    id : String,
-    name : String,
-    color : String,
-    size : String,
-    quantity : Number,
-    detail : String,
-    image : String
+  id: String,
+  name: String,
+  color: String,
+  size: String,
+  quantity: Number,
+  detail: String,
+  image: String
 })
 
 module.exports = wearSchema
