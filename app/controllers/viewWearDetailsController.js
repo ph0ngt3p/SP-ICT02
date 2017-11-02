630b0d2b46ba06053551bedd2183f2c292c74df2
Tuấn Dũng <zun1903@gmail.com>
2017-11-02 16:56:24 +0700

    catch exception when a random id is entered into the param

--- app/controllers/viewWearDetailsController.js
+++ app/controllers/viewWearDetailsController.js
@@ -1,14 +1,21 @@
 'use strict'
 
+const logger = require('winston')
 const compose = require('koa-compose')
 const models = require('../models/mongoose')
 
 async function viewWearDetails (ctx) {
   const { id } = ctx.params
-  const item = await models.Wears.getItemDetails(id)
-  return ctx.render('item', {
-    item
-  })
+  let item
+  try {
+    item = await models.Wears.getItemDetails(id)
+    return ctx.render('item', {
+      item
+    })
+  } catch (e) {
+    logger.error('Error happened: Wrong id entered!')
+    return ctx.render('404')
+  }
 }
 
 module.exports = compose([
