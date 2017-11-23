58443b0ecb704eef78c15ebb1441cef74d26a572
tuandzung <zun1903@gmail.com>
2017-11-23 10:53:15 +0700

    refactor mongoose singleton

--- app/models/mongoose/index.js
+++ app/models/mongoose/index.js
@@ -1,12 +1,31 @@
 'use strict'
 
-const connect = require('./db')
+const mongoose = require('mongoose')
+const logger = require('winston')
+const config = require('../../config')
 const Users = require('./Users')
 const Wears = require('./Wears')
 const Cart = require('./Cart')
 
+mongoose.Promise = config.mongodb.promiseLibrary
+mongoose.connect(config.mongodb.uri, config.mongodb.driverOptions)
+
+mongoose.connection.on('connected', () => {
+  logger.info('Mongoose successfully connected to', config.mongodb.uri)
+})
+
+mongoose.connection.on('error', (err) => {
+  logger.error(`Error happened during establishing connection to MongoDb, ${err}`)
+})
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    logger.warn('Mongoose connection is disconnected due to application termination')
+    process.exit(0)
+  })
+})
+
 module.exports = {
-  connect,
   Users,
   Wears,
   Cart
